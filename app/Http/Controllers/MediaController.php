84e40f86f63115b1144d6dc5eb343608712039f6
kristoffabjan <kristof.fabjan@gmail.com>
2024-05-02 11:58:37 +0200

    Save media file to disk

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\JsonResponse;
 
 class MediaController extends Controller
 {
-    public function store(MediaUploadRequest $request) : JsonResponse {
+    public function store(MediaUploadRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+        
+        $file = $request->file('media');
+        $filename = time() . '_' . $file->getClientOriginalName();
+        $file->storeAs('media', $filename);
 
         return response()->json([
             'test' => "test"
